f890f618380cfdc2825b856fa26d7693d7765cd5
Jean gonçalves Lima <cbjeanlima@gmail.com>
2023-06-25 20:38:08 -0300

    :bug: fix: bug no input de pesquisa, atribuido o Id que chama a função de pesquisa

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// Lista de páginas disponíveis no seu restaurante
+// Lista de restaurantes disponíveis no nosso site
 const paginasRestaurante = [
     { titulo: 'Sabores do Sertão', url: '/restaurante_individual.html?restaurante=1' },
     { titulo: 'Brasa & Sabor', url: '/restaurante_individual.html?restaurante=2' },
@@ -6,7 +6,7 @@ const paginasRestaurante = [
     { titulo: 'Sabor Carioca', url: '/restaurante_individual.html?restaurante=4' },
     { titulo: 'Cozinha do Brasil', url: '/restaurante_individual.html?restaurante=5' },
     { titulo: 'Comida de Buteco', url: '/restaurante_individual.html?restaurante=6' },
-    // Adicione mais páginas do seu restaurante conforme necessário
+    // Aqui é possivel adiionar mais páginas de restaurantes no nosso site
   ];
   
   function realizarPesquisaRestaurantes() {
@@ -36,5 +36,45 @@ const paginasRestaurante = [
     } else {
       resultado.textContent = 'Nenhum resultado encontrado.';
     }
+  };
+
+
+  // Lista de restaurantes disponíveis no nosso site
+const paginasReceitas = [
+  { titulo: 'Tacacá do Norte', url: '/receita_individual.html?receita=1' },
+  { titulo: 'Vatapá', url: '/receita_individual.html?receita=2' },
+  { titulo: 'Galinhada', url: '/receita_individual.html?receita=3' },
+  { titulo: 'Escondidinho', url: '/receita_individual.html?receita=4' },
+  { titulo: 'Moqueca', url: '/receita_individual.html?receita=5' },
+  // Aqui é possivel adiionar mais páginas de restaurantes no nosso site
+];
+
+function realizarPesquisaReceitas() {
+  const valorPesquisa = document.getElementById('pesquisaReceita').value.toLowerCase();
+  const resultado = document.getElementById('resultadoReceita');
+
+  // Limpa o resultado anterior
+  resultado.innerHTML = '';
+
+  // Realiza a pesquisa
+  const resultadosEncontrados = paginasReceitas.filter(pagina => {
+    const titulo = pagina.titulo.toLowerCase();
+    const url = pagina.url.toLowerCase();
+
+    // Verifica se o valor de pesquisa está presente no título ou na URL da página
+    return titulo.includes(valorPesquisa) || url.includes(valorPesquisa);
+  });
+
+  // Exibe os resultados da pesquisa
+  if (resultadosEncontrados.length > 0) {
+    resultadosEncontrados.forEach(pagina => {
+      const link = document.createElement('a');
+      link.href = pagina.url;
+      link.textContent = pagina.titulo;
+      resultado.appendChild(link);
+    });
+  } else {
+    resultado.textContent = 'Nenhum resultado encontrado.';
   }
+}
   
